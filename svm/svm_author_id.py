cd5a09214e7ee36a35dbed74269585f8ee9d4f39
nesreen sada <nesreensada@gmail.com>
2019-01-20 14:13:09 +0200

    add svm module

---
+++ svm/svm_author_id.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 2 (SVM) mini-project.
+
+    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
+
+from sklearn.svm import SVC
+clf = SVC(kernel='rbf',C=10000)
+
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t1 = time()
+predict = clf.predict(features_test)
+print "training time:", round(time()-t1, 3), "s"
+#########################################################
+### your code goes here ###
+accuracy = clf.score(features_test, labels_test)
+print accuracy
+#########################################################
+print(sum(predict),'how many chris')
+
+
